921350fb427133a2c6ce4e2c26722472ca1445f6
prajwalajayaprakash <prajwalaprakash09@gmail.com>
2017-07-05 23:52:03 -0400

    Adding files

---
+++ JavaScript/whileLoops.js
@@ -0,0 +1,35 @@
+console.log("Print all numbers between -10 and 19");
+var x= -10;
+while(x<=19){
+console.log(x);
+x++;
+}
+
+console.log("Print all even numbers between 10 and 40");
+var y=10;
+while(y<=40){
+	if(y%2==0){
+		console.log(y);
+		}
+	y++;
+}
+
+console.log("Print all odd numbers between 300 and 333");
+var z= 300;
+while(z<=333){
+	if(z%2==1){
+		console.log(z);
+
+	}
+	z++;
+}
+
+console.log("Print all numbers divisible by 5 and 3 between 5 and 50");
+var w=5;
+while(w<=50){
+	if(w%3==0 && w%5==0){
+		console.log(w);
+	}
+	w++;
+}
+
